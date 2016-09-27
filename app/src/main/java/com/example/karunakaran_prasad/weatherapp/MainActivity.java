4e384d02b63ec493618286a96aafbd50c753ee64
Karunakaran Prasad <kprasad@Karunakarans-MacBook-Air.local>
2016-09-26 22:28:14 -0700

    Added an options menu + Fetch data over the web with background task (AsyncTask)

--- app/src/main/java/com/example/karunakaran_prasad/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/karunakaran_prasad/weatherapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.karunakaran_prasad.weatherapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d("MainActivity", "onCreate - setting content view");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/karunakaran_prasad/weatherapp/MainFragment.java
+++ app/src/main/java/com/example/karunakaran_prasad/weatherapp/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.karunakaran_prasad.weatherapp;
 
+import android.os.AsyncTask;
 import android.view.View;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,8 +10,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.example.karunakaran_prasad.weatherapp.R;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +25,8 @@ import java.util.List;
  */
 public class MainFragment extends Fragment {
 
+
+    public static final String URL_STRING = "http://api.openweathermap.org/data/2.5/forecast/daily?id=5375480&cnt=7&APPID=e3226c55f0b50d304e1a7e408117354e&units=metric";
 
     public MainFragment() {
         // Required empty public constructor
@@ -51,7 +58,91 @@ public class MainFragment extends Fragment {
         ListView forecast_list_view = (ListView) root_view.findViewById (R.id.listview_forecast);
         forecast_list_view.setAdapter(mForeCastAdapter);
 
+        new FetchWeatherTask().execute(URL_STRING);
+
+
         return root_view;
     }
 
+    private class FetchWeatherTask extends AsyncTask<String, Integer, Long> {
+
+        @Override
+        protected Long doInBackground(String... args){
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            String url_weather = args[0];
+            Log.d("MainFragment", "FetchWeatherTask: doInBackground : "  + url_weather);
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL(url_weather);
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+                Log.d("MainFragment", forecastJsonStr);
+            } catch (IOException e) {
+                Log.e("MainFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("MainFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+        return 0L;//for success
+        }//End of doInBackground
+
+        protected void onPostExecute(Long result) {
+            Log.d("MainFragment", "Done executing in the background");
+            //Toast.makeText(this, "Downloaded " + result + " bytes");
+            //return result;
+        }
+
+    }//End of async task
+
 }
+
+//http://api.openweathermap.org/data/2.5/forecast/daily?id=5375480&cnt=7&APPID=e3226c55f0b50d304e1a7e408117354e
+//http://api.openweathermap.org/data/2.5/forecast/daily?id=5375480&cnt=7&APPID=e3226c55f0b50d304e1a7e408117354e&units=metric
